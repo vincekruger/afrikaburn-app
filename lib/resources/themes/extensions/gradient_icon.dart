2cc92d068dcdc05b24f112e574293e870def8ec4
Vince Kruger <vincekruger@gmail.com>
2024-04-03 19:48:38 +0200

    new extensions for icon buttons

--- lib/resources/themes/extensions/gradient_icon.dart
+++ lib/resources/themes/extensions/gradient_icon.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// [Text] Extensions
+/// [Icon] Extensions
 extension GradientIcon on Icon {
   ShaderMask withGradeint(Gradient gradient) {
     return ShaderMask(
@@ -9,6 +9,51 @@ extension GradientIcon on Icon {
         this.icon,
         size: this.size,
         color: Colors.white,
+      ),
+    );
+  }
+}
+
+extension GradientButton on IconButton {
+  Widget withGradient(Gradient gradient) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: gradient,
+        borderRadius: BorderRadius.circular(50.0),
+      ),
+      padding: EdgeInsets.zero,
+      child: IconButton(
+        padding: EdgeInsets.zero,
+        visualDensity: VisualDensity.compact,
+        icon: this.icon,
+        onPressed: this.onPressed,
+      ),
+    );
+  }
+
+  Widget withGradientAndIconOverlay(Gradient gradient,
+      {required Icon overlayIcon, double? iconTop, double? iconLeft}) {
+    return InkWell(
+      onTap: this.onPressed,
+      borderRadius: BorderRadius.circular(50.0),
+      child: Stack(
+        children: [
+          Container(
+            width: overlayIcon.size ?? 40,
+            height: overlayIcon.size ?? 40,
+            decoration: BoxDecoration(
+              gradient: gradient,
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            padding: EdgeInsets.zero,
+            child: this.icon,
+          ),
+          Positioned(
+            top: iconTop ?? 0,
+            left: iconLeft ?? 0,
+            child: overlayIcon,
+          ),
+        ],
       ),
     );
   }
