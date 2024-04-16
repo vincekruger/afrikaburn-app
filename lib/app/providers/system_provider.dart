cf8221ad1976ff9e2907721916abb7de7fcdb86c
Vince Kruger <vincekruger@gmail.com>
2024-04-16 15:35:15 +0200

    prepare system directories on boot

--- lib/app/providers/system_provider.dart
+++ lib/app/providers/system_provider.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:afrikaburn/app/models/ticket.dart';
+import 'package:path/path.dart' as p;
 import 'package:flutter/services.dart';
 import 'package:nylo_framework/nylo_framework.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:afrikaburn/app/models/guide.dart';
 
 class SystemProvider implements NyProvider {
   boot(Nylo nylo) async {
@@ -7,7 +13,29 @@ class SystemProvider implements NyProvider {
     return nylo;
   }
 
-  afterBoot(Nylo nylo) async {}
+  afterBoot(Nylo nylo) async {
+    await prepareDocumentDirectories();
+  }
+
+  /// Prepare the document directories when the system boots
+  Future<void> prepareDocumentDirectories() async {
+    final Directory appDocumentsDir = await getApplicationDocumentsDirectory();
+    final Directory appLibraryDir =
+        Platform.isIOS ? await getLibraryDirectory() : appDocumentsDir;
+
+    /// Tickets Directory
+    final Directory ticketDir =
+        Directory(p.join(appDocumentsDir.path, ticketLocalPath));
+    final Directory ticketThumbnailDir =
+        Directory(p.join(appLibraryDir.path, ticketThumbnailLocalPath));
+    if (!ticketDir.existsSync()) ticketDir.createSync();
+    if (!ticketThumbnailDir.existsSync()) ticketThumbnailDir.createSync();
+
+    /// Guides Directory
+    final Directory guidesDir =
+        Directory(p.join(appDocumentsDir.path, guideLocalPath));
+    if (!guidesDir.existsSync()) guidesDir.createSync();
+  }
 
   /// Set only portrait orientation
   setOnlyPortraitOrientation() async {
